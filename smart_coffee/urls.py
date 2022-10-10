683aba23af6f8f0d29bbd3598f8b4876078768af
david <brownidj@gmail.com>
2022-10-10 10:05:29 +1000

    Page numbers

--- smart_coffee/urls.py
+++ smart_coffee/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from smart_coffee import views
 
 urlpatterns = [
-    path('<int:num_page>', views.num_page_view, name='num_page_view'),
-    path('<str:page>', views.page_view, name='page_view')
+    path("", views.example_view),
+    # path('<int:num_page>', views.num_page_view, name='num_page_view'),
+    # path('<str:page>', views.page_view, name='page_view')
 ]
--- smart_coffee/views.py
+++ smart_coffee/views.py
@@ -1,10 +1,12 @@
 from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.shortcuts import render
 from django.urls import reverse
 
 pages = {
+    'news': '<h1>News</h1>>',
+    'routes': '<h1>Routes</h1>>',
     'shops': '<h1>Coffee Shops</h1>',
     'volunteers': '<h1>Volunteers</h1>',
-    'routes': '<h1>Routes</h1>>'
 }
 
 
@@ -25,3 +27,8 @@ def num_page_view(request, num_page):
         return HttpResponseRedirect(webpage)
     except:
         raise Http404('404 Generic Error')
+
+
+def example_view(request):
+    my_variable_dict = {'first_name': 'Rosalind', 'last_name': 'Franklin'}
+    return render(request, 'smart_coffee/example.html', context=my_variable_dict)
